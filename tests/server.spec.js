dd06784db5fcd468bb518486d3192ebbff73db30
Laurel Kelly <laurellkelly@gmail.com>
2020-08-17 15:15:33 -0700

    Update server.spec.js

--- tests/server.spec.js
+++ tests/server.spec.js
@@ -37,4 +37,14 @@ describe('server/app.js', function() {
       done();
     });
   });
+
+  it('page says "All tests are passing, huzzah!"', (done) => {
+  chai.request(server)
+    .get('/')
+    .end((err, res) => {
+      expect(err).not.exist;
+      expect(JSON.stringify(res.text)).to.contain('All tests are passing, huzzah!');
+      done();
+    });
+  });
 })
